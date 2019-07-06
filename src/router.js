d678330af0e6b587675da98814b905705de398fe
姚林 <yaolin1@xiaomi.com>
2019-07-06 16:34:14 +0800

    多级选择

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Test from "./views/Test.vue";
 import e1 from "./views/examples/e1.vue";
+import e2 from "./views/examples/e2.vue";
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: "/e1",
       name: "e1",
       component: e1
+    },
+    {
+      path: "/e2",
+      name: "e2",
+      component: e2
     }
   ]
 });
